a83572a47d19e62e24ceccef82c1ed0b611a7c1e
Brady <goldsworthybrady@gmail.com>
2019-04-01 10:56:21 -0500

    Mama mia

---
+++ app/src/main/java/com/example/portfoliio11/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.portfoliio11;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private Button[][] buttons;
+
+    private TicTacToe game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main); //we aren't using activity_main.xml
+
+        //Create TicTacToe game
+
+        game = new TicTacToe();
+
+        buildGUI();
+
+
+
+    } //end onCreate
+
+    public void buildGUI() {
+        //Get width of screen
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+
+        //Buttons will be 1/3 size of screen
+        int width = size.x / TicTacToe.SIDE;
+
+        //Create a grid layout
+        GridLayout gridLayout = new GridLayout(this);
+
+        //Set rows and cols of grid
+        gridLayout.setRowCount(TicTacToe.SIDE);
+        gridLayout.setColumnCount(TicTacToe.SIDE);
+
+        //Create 2D array of buttons
+        buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
+
+        //handle Button clicks
+        ButtonHandler buttonHandler = new ButtonHandler();
+
+        //Place buttons in grid
+        for (int row = 0; row < TicTacToe.SIDE; row++) {
+            for (int col = 0; col < TicTacToe.SIDE; col++) {
+                //generate button for each spot in button array
+                buttons[row][col] = new Button(this);
+
+                //set textSize for X's and O's
+                buttons[row][col].setTextSize((int)(width * 0.2));
+
+                buttons[row][col].setOnClickListener(buttonHandler);
+
+                //Add button to gridLayout
+                gridLayout.addView(buttons[row][col], width, width); //button is a width x width square
+            }
+        }
+
+        setContentView(gridLayout); //Render button array
+
+
+
+
+
+    } //end buildGUI
+
+    //The  button handler
+    private class ButtonHandler implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            //Display message on click
+            //Toast.makeText(MainActivity.this, "ButtonHandler onCLick", Toast.LENGTH_SHORT).show();
+
+            //determine which button was clicked
+            for (int row = 0; row < TicTacToe.SIDE; row++) {
+                for (int col = 0; col < TicTacToe.SIDE; col++) {
+                    if (v == buttons[row][col]) {
+                        update(row, col);
+                    }
+
+                }
+            }
+
+        } //end onCLick
+
+
+
+    } //end buttonhandler
+
+
+    public void update(int row, int col) {
+        //Display message on update
+        Toast.makeText(this, "Button updated, row: " + row + " col: " + col, Toast.LENGTH_SHORT).show();
+
+        //find the current player
+        int currentPlayer = game.play(row, col);
+
+        if (currentPlayer == 1) {
+            //Put symbol onto button
+            buttons[row][col].setText("X");
+        }
+        else if (currentPlayer == 2){
+            buttons[row][col].setText("O");
+        }
+
+        //check if game is over
+        if (game.isGameOver()) {
+            enableButtons(false);
+        }
+
+
+    } //end update
+
+    public void enableButtons(boolean enabled) {
+        for (int row = 0; row < TicTacToe.SIDE; row++) {
+            for (int col = 0; col < TicTacToe.SIDE; col++) {
+                buttons[row][col].setEnabled(enabled);
+
+            }
+        }
+
+    }
+
+
+}
